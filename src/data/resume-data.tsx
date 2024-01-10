fea4d5f11da38fa025bc41e345ccc0fdfb219619
Michael Bushoy <mbushoy@gmail.com>
2024-01-09 19:40:30 -0500

    adapt cv template & content
    content
    
    favicons
    
    formatting
    
    readme
    
    title

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,207 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
-export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+import { CoinbaseLogo } from "@/images/logos";
+
+type ResumeDataType = {
+  name: string;
+  initials: string;
+  location: string;
+  locationLink: string;
+  about?: string;
+  summary: string;
+  avatarUrl?: string;
+  personalWebsiteUrl: string;
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: string;
+    tel: string;
+    social: {
+      name: string;
+      url: string;
+      icon: React.ComponentType<{ className: string }>;
+    }[];
+  };
+  education: {
+    school: string;
+    degree: string;
+    start: string;
+    end: string;
+  }[];
+  work: {
+    company: string;
+    link: string;
+    badges?: string[];
+    title: string;
+    logo?: string;
+    start?: string;
+    end?: string;
+    description?: string;
+    roles?: {
+      title: string;
+      description?: string | string[];
+      start?: string;
+      end?: string;
+    }[];
+  }[];
+  skills: string[];
+  focusAreas: string[];
+  projects: {
+    title: string;
+    description: string;
+    techStack: string[];
+    link: {
+      href: string;
+      text: string;
+    };
+  }[];
+};
+
+export const RESUME_DATA: ResumeDataType = {
+  name: "Michael Bushoy",
+  initials: "MB",
+  location: "Brooklyn, NY",
+  locationLink: "https://www.google.com/maps/place/Brooklyn",
+  summary:
+    "Software engineer & tech lead specializing in developing web applications with the user in mind",
+  avatarUrl: "https://avatars.githubusercontent.com/u/12397314?v=4",
+  personalWebsiteUrl: "https://michael.bushoy.com",
+  contact: {
+    email: "mbushoy@gmail.com",
+    tel: "+16178693800",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/mb389",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/michaelbushoy",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/mb389",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Cornell University",
+      degree: "Bachelor's Degree in Industrial and Labor Relations",
+      start: "2004",
+      end: "2007",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Coinbase",
+      link: "https://www.coinbase.com",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
+      title: "Senior Software Engineer",
+      logo: CoinbaseLogo,
+      start: "2019",
       end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+      roles: [
+        {
+          title: "Cloud Platform, UI Lead",
+          description: [
+            "Lead team of 6 engineers on creation of Coinbase`s developer platform, shipping support for recurring billing, organization & project management, API key management, and more.",
+          ],
+        },
+        {
+          title: "Protocols, UI Lead",
+          description: [
+            "First frontend hire on a new team making Coinbase`s first foray into building crypto protocols. Led work across Coinbase web & mobile apps as well as greenfield initiatives. Shut down Aug 2022 as part of company-wide cost cutting initiatives.",
+          ],
+        },
+        {
+          title: "Retail Advanced Trading, Web Lead",
+          description: [
+            "Led the build-out of the Advanced Trading product, the successor to Coinbase Pro, to a successful public launch. It is now responsible for processing most of Coinbase`s $100bn+ annual consumer trading volume. ",
+            "Acted as lead engineer, project manager, primary liaison with product/design/backend, and mentor to junior engineers.",
+            "Made & documented key architectural decisions around data layer implementation, charting approach, and general library/stack selection and presented to org-wide engineering review.",
+            "Built key pages/components such as the Price Chart, Order Form, and Orders Page.",
+          ],
+        },
+        {
+          title: "Coinbase Pro",
+          description: [
+            "Contributed to Pro web and mobile app development. Projects included adding multi-portfolio support, re-writing the transfer flow, and leading a dedicated sprint to optimize React Native mobile app performance.",
+          ],
+        },
+      ],
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Fundera (acq. by NerdWallet)",
+      link: "https://www.fundera.com",
+      badges: ["New York, NY"],
+      title: "Software Engineer",
+      start: "2018",
+      end: "2019",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "As part of Fundera`s Conversion team, focused on enhancing user onboarding & registration, improving lender-partner API integrations, and maintaining Treehouse, the internal admin & CRM platform.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Domio",
+      link: "https://www.crunchbase.com/organization/domio-inc",
+      badges: ["New York, NY"],
+      title: "Software Engineer",
+      start: "2017",
+      end: "2018",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Built out Domio`s Booking Platform, a web application that allowed users to search properties, create & manage reservations, and make payments. Integrated with Airbnb, Guesty, and Stripe.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      company: "IrisVR (acq. by Autodesk)",
+      link: "https://www.irisvr.com",
+      badges: ["New York, NY"],
+      title: "Software Engineer",
+      start: "2016",
+      end: "2017",
+      description:
+        "Maintained and enhanced IrisVR`s web application and related services, with a focus on the Scope Library, a tool allowing users to upload, manage, & preview panoramic images. Led development of account settings section, payment flow, & multi-format support for panoramic image uploading.      ",
+    },
+    {
+      company: "JPMorgan",
+      link: "https://www.jpmorgan.com",
+      badges: ["New York, NY"],
+      title: "Vice President",
+      roles: [
+        {
+          title: "Market Risk Management",
+          start: "2009",
+          end: "2015",
+        },
+        {
+          title: "Rotational Analyst, Internal Consulting Services",
+          start: "2007",
+          end: "2009",
+        },
+      ],
     },
   ],
   skills: [
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React",
+    "React Native",
+    "Webpack",
     "Node.js",
     "GraphQL",
     "Relay",
-    "WebRTC",
+    "Ruby",
+    "Go",
+    "Python",
+    "Datadog",
   ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+  focusAreas: [
+    "UI development",
+    "frontend architecture",
+    "performance optimization",
+    "data layer & state management design",
+    "real-time data & interaction",
+    "PWAs (progressive web apps)",
   ],
+  projects: [],
 } as const;
